845ae57566bb32111a7eb51d4622819d0578e195
Wasim <wasimxaman13@gmail.com>
2023-08-14 17:56:33 +0500

    component created

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:gtrack_mobile_app/constants/app_icons.dart';
 import 'package:gtrack_mobile_app/constants/app_images.dart';
 import 'package:gtrack_mobile_app/global/common/colors/app_colors.dart';
@@ -114,68 +113,57 @@ class NavigateIconWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Material(
-        elevation: 10,
-        borderRadius: BorderRadius.circular(10),
-        color: AppColors.background,
-        child: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          width: MediaQuery.of(context).size.width * 0.9,
-          height: context.height * 0.17,
-          child: Stack(
+    return Card(
+      color: AppColors.background,
+      elevation: 7,
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            // Leading Image
             children: [
-              ListTile(
-                onTap: onTap,
-                dense: true,
-                leading: Image.asset(
-                  icon,
-                  width: 50,
-                  fit: BoxFit.cover,
-                ),
-                title: AutoSizeText(
-                  caption,
-                  minFontSize: 10,
-                  maxFontSize: 10,
-                ),
-                subtitle: AutoSizeText(
-                  title,
-                  maxFontSize: 25,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  minFontSize: 20,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: color,
+              Container(
+                padding: const EdgeInsets.all(5),
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  color: color,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: color),
+                  image: DecorationImage(
+                    image: AssetImage(icon),
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              Positioned(
-                left: 0,
-                top: 0,
-                bottom: 0,
-                child: Container(
-                  padding: const EdgeInsets.all(5),
-                  width: 70,
-                  height: 70,
-                  decoration: BoxDecoration(
-                    color: color,
-                    shape: BoxShape.circle,
-                    border: Border.all(color: color),
-                    image: DecorationImage(
-                      image: AssetImage(icon),
-                      fit: BoxFit.cover,
+              const SizedBox(width: 20),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AutoSizeText(
+                      caption,
+                      minFontSize: 12,
+                      maxFontSize: 12,
                     ),
-                  ),
+                    const SizedBox(height: 5),
+                    AutoSizeText(
+                      title,
+                      maxFontSize: 25,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      minFontSize: 20,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: color,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
-          ),
-        ),
-      ),
+          )),
     );
   }
 }
