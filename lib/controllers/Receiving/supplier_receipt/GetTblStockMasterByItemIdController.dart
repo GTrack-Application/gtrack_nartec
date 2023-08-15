f5d7cae04b107e3023bb9e4781a2adf2a5e7dfd9
faysalzaman <faysalzaman00@gmail.com>
2023-08-15 08:03:12 -0700

    Updated

--- lib/controllers/Receiving/supplier_receipt/GetTblStockMasterByItemIdController.dart
+++ lib/controllers/Receiving/supplier_receipt/GetTblStockMasterByItemIdController.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, avoid_print
 
 import 'dart:convert';
 
@@ -23,17 +23,21 @@ class GetTblStockMasterByItemIdController {
       var response = await http.get(uri, headers: headers);
 
       if (response.statusCode == 200) {
+        print("Status Code: ${response.statusCode}");
+
         var data = json.decode(response.body) as List;
         List<GetTblStockMasterByItemIdModel> shipmentData = data
             .map((e) => GetTblStockMasterByItemIdModel.fromJson(e))
             .toList();
         return shipmentData;
       } else {
+        print("Status Code: ${response.statusCode}");
         var data = json.decode(response.body);
         var msg = data['message'];
         throw Exception(msg);
       }
     } catch (e) {
+      print(e);
       throw Exception(e);
     }
   }
