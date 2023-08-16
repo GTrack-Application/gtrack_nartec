de985869b53ae75a743ef076eab84a2f5cd67382
faysalzaman <faysalzaman00@gmail.com>
2023-08-16 00:29:39 -0700

    Update

---
+++ lib/screens/home/identify/GLN/gln_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GLNScreen extends StatefulWidget {
+  const GLNScreen({super.key});
+
+  @override
+  State<GLNScreen> createState() => _GLNScreenState();
+}
+
+class _GLNScreenState extends State<GLNScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/home/identify/identify_screen.dart
+++ lib/screens/home/identify/identify_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gtrack_mobile_app/constants/app_icons.dart';
 import 'package:gtrack_mobile_app/global/common/colors/app_colors.dart';
+import 'package:gtrack_mobile_app/global/common/utils/app_navigator.dart';
+import 'package:gtrack_mobile_app/screens/home/identify/GLN/gln_screen.dart';
 import 'package:gtrack_mobile_app/screens/home/widgets/card_icon_button.dart';
 
 class IdentifyScreen extends StatefulWidget {
@@ -16,43 +18,58 @@ class _IdentifyScreenState extends State<IdentifyScreen> {
       "text": "GTIN",
       "icon": AppIcons.gtin,
       "desctiption": "Global Trade\nItem Number",
+      "onTap": () {},
     },
     {
       "text": "GLN",
       "icon": AppIcons.gln,
       "desctiption": "Global Location\nNumber",
+      "onTap": () {},
     },
     {
       "text": "SSCC",
       "icon": AppIcons.sscc,
       "desctiption": "Serial Shipping\nContainer Code",
+      "onTap": () {},
     },
     {
       "text": "GIAI",
       "icon": AppIcons.giai,
       "desctiption": "Global Individual\nAsset Identifier",
+      "onTap": () {},
     },
     {
       "text": "GRAI",
       "icon": AppIcons.grai,
       "desctiption": "Global Returnable\nAsset Identifier",
+      "onTap": () {},
     },
     {
       "text": "GMN",
       "icon": AppIcons.gmn,
       "desctiption": "Global Model\nNumber",
+      "onTap": () {},
     },
     {
       "text": "GSRN",
       "icon": AppIcons.gsrn,
       "desctiption": "Global Service\nRelation Number",
+      "onTap": () {},
     },
     {
       "text": "GSIN",
       "icon": AppIcons.gsin,
       "desctiption": "Global Shipment\nIdentification Number",
+      "onTap": () {},
     }
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    data[1]["onTap"] = () =>
+        AppNavigator.goToPage(context: context, screen: const GLNScreen());
+  }
 
   final gridDelegate = const SliverGridDelegateWithFixedCrossAxisCount(
     crossAxisCount: 2,
@@ -82,7 +99,7 @@ class _IdentifyScreenState extends State<IdentifyScreen> {
                 itemBuilder: (context, index) {
                   return CardIconButton(
                     icon: data[index]["icon"] as String,
-                    onPressed: () {},
+                    onPressed: data[index]["onTap"],
                     text: data[index]['text'] as String,
                   );
                 },
