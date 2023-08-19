18bf4cfac7bca442eeb5f2839438c2d74e3809d8
Wasim <wasimxaman13@gmail.com>
2023-08-19 15:58:33 +0500

    mapping new icon added to the capture screen

--- lib/screens/capture/capture_screen.dart
+++ lib/screens/capture/capture_screen.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:gtrack_mobile_app/constants/app_icons.dart';
 import 'package:gtrack_mobile_app/global/common/colors/app_colors.dart';
 import 'package:gtrack_mobile_app/global/common/utils/app_navigator.dart';
+import 'package:gtrack_mobile_app/global/widgets/buttons/card_icon_button.dart';
 import 'package:gtrack_mobile_app/screens/capture/Aggregation/aggregation_screen.dart';
 import 'package:gtrack_mobile_app/screens/capture/Association/association_screen.dart';
 import 'package:gtrack_mobile_app/screens/capture/Transformation/transformation_screen.dart';
-import 'package:gtrack_mobile_app/global/widgets/buttons/card_icon_button.dart';
 
 class CaptureScreen extends StatefulWidget {
   const CaptureScreen({super.key});
@@ -38,6 +38,11 @@ class _CaptureScreenState extends State<CaptureScreen> {
     },
     {
       "text": "MAPPING BARCODE",
+      "icon": AppIcons.mapping,
+      "onTap": () {},
+    },
+    {
+      "text": "MAPPING RFID",
       "icon": AppIcons.mapping,
       "onTap": () {},
     },
