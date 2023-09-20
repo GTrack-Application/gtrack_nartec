280ed37b5075b28a8a85269c13c097c97de71760
Wasim <wasimxaman13@gmail.com>
2023-09-20 12:53:42 +0500

    events screen updated with the google map

--- lib/screens/home/share/product-information/events_screen.dart
+++ lib/screens/home/share/product-information/events_screen.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:gtrack_mobile_app/controllers/share/product_information/events_screen_controller.dart';
 import 'package:gtrack_mobile_app/global/common/colors/app_colors.dart';
@@ -19,21 +22,57 @@ class EventsScreen extends StatefulWidget {
 }
 
 class _EventsScreenState extends State<EventsScreen> {
-  List<bool> isMarked = [];
-
   List<double> longitude = [];
   List<double> latitude = [];
 
   double currentLat = 0;
   double currentLong = 0;
 
-  // markers
-  Set<Marker> markers = {};
-
   bool isLoaded = false;
 
   // flag to show the table
   bool isTableVisible = false;
+
+  List<Polyline> yourPolylinesList = [];
+  List<LatLng> polylineCoordinates = [];
+  PolylinePoints polylinePoints = PolylinePoints();
+  List<Marker> yourMarkersList = [];
+
+  // Origin and destination
+  PointLatLng? origin;
+  PointLatLng? destination;
+
+  void setPolylines(double lat, double long, int i) async {
+    origin = PointLatLng(latitude[i], longitude[i]);
+    destination = PointLatLng(lat, long);
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      "AIzaSyCsEUxB9psxb-LxhYx8hJtF248gj4bx49A", // Replace with your Google Maps API key
+      origin!, // Starting coordinates
+      destination!, // Ending coordinates
+      avoidFerries: true,
+      avoidHighways: false,
+      avoidTolls: true,
+      travelMode: TravelMode.driving,
+      optimizeWaypoints: true,
+    );
+
+    if (result.status == 'OK') {
+      for (var point in result.points) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      }
+
+      Polyline polyline = Polyline(
+        polylineId: const PolylineId('polyline'),
+        color: AppColors.green,
+        points: polylineCoordinates,
+        width: 3,
+      );
+
+      setState(() {
+        yourPolylinesList.add(polyline);
+      });
+    }
+  }
 
   @override
   void initState() {
@@ -47,7 +86,6 @@ class _EventsScreenState extends State<EventsScreen> {
             : widget.gtin.substring(1, 14);
         EventsScreenController.getEventsData(gtin).then((value) {
           setState(() {
-            isMarked = List.filled(value.length, true);
             table = value;
             latitude = value
                 .map((e) => double.parse(e.itemGPSOnGoLat.toString()))
@@ -56,11 +94,12 @@ class _EventsScreenState extends State<EventsScreen> {
                 .map((e) => double.parse(e.itemGPSOnGoLon.toString()))
                 .toList();
 
-            currentLat = 24.7136;
-            currentLong = 46.6753;
+            currentLat = latitude[0];
+            currentLong = longitude[0];
 
-            // setting up markers
-            markers = table.map((data) {
+            // setPolylines(latitude[1], longitude[1], 0);
+
+            yourMarkersList = table.map((data) {
               return Marker(
                 markerId: MarkerId(data.memberID.toString()),
                 position: LatLng(
@@ -72,14 +111,19 @@ class _EventsScreenState extends State<EventsScreen> {
                   snippet: "${data.itemGPSOnGoLat}, ${data.itemGPSOnGoLon}",
                 ),
               );
-            }).toSet();
+            }).toList();
 
+            polylineCoordinates = polylineCoordinates.toSet().toList();
+
+            // connect each and every point with each other other but only using routes
+            for (int i = 0; i < table.length - 2; i++) {
+              setPolylines(latitude[i + 1], longitude[i + 1], i);
+            }
             isLoaded = true;
           });
           AppDialogs.closeDialog();
         }).onError((error, stackTrace) {
           setState(() {
-            isMarked = List.filled(0, false);
             table = [];
           });
           AppDialogs.closeDialog();
@@ -99,7 +143,6 @@ class _EventsScreenState extends State<EventsScreen> {
   void dispose() {
     mapController.dispose();
 
-    markers.clear();
     latitude.clear();
     longitude.clear();
     table.clear();
@@ -115,7 +158,6 @@ class _EventsScreenState extends State<EventsScreen> {
           : Column(
               children: [
                 ElevatedButton(
-                  child: Text(isTableVisible ? "Hide Grid" : "Show Grid"),
                   onPressed: () {
                     setState(() {
                       isTableVisible = !isTableVisible;
@@ -124,23 +166,93 @@ class _EventsScreenState extends State<EventsScreen> {
                   style: ElevatedButton.styleFrom(
                     backgroundColor: AppColors.green,
                   ),
+                  child: Text(isTableVisible ? "Hide Grid" : "Show Grid"),
                 ),
                 !isTableVisible
                     ? const SizedBox.shrink()
-                    : PaginatedDataTable(
-                        columns: const [
-                          DataColumn(label: Text("Event Id")),
-                          DataColumn(label: Text("Member Id")),
-                          DataColumn(label: Text("Ref Description")),
-                          DataColumn(label: Text("Date Created")),
-                          DataColumn(label: Text("Date Last Updated")),
-                          DataColumn(label: Text("GLN Id From")),
-                          DataColumn(label: Text("GLN Id To")),
-                        ],
-                        source: EventsSource(),
-                        arrowHeadColor: AppColors.green,
-                        showCheckboxColumn: false,
-                        rowsPerPage: 3,
+                    : Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: AppColors.green),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: PaginatedDataTable(
+                          columns: const [
+                            DataColumn(
+                                label: Text(
+                              "Event Id",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.green,
+                              ),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              "Member Id",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.green,
+                              ),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              "Ref Description",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.green,
+                              ),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              "Date Created",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.green,
+                              ),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              "Date Last Updated",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.green,
+                              ),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              "GLN Id From",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.green,
+                              ),
+                            )),
+                            DataColumn(
+                                label: Text(
+                              "GLN Id To",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.green,
+                              ),
+                            )),
+                          ],
+                          source: EventsSource(),
+                          arrowHeadColor: AppColors.green,
+                          showCheckboxColumn: false,
+                          rowsPerPage: 3,
+                        ).visible(isTableVisible),
                       ).visible(isTableVisible),
                 Expanded(
                   child: GoogleMap(
@@ -149,14 +261,8 @@ class _EventsScreenState extends State<EventsScreen> {
                       mapController = controller;
                     },
                     initialCameraPosition: CameraPosition(
-                      // with current position using geolocator
-                      target: latitude.isEmpty
-                          ? LatLng(currentLat, currentLong)
-                          : LatLng(
-                              latitude[0],
-                              longitude[0],
-                            ),
-                      zoom: 14,
+                      target: LatLng(currentLat, currentLong),
+                      zoom: 12,
                       tilt: 0,
                       bearing: 0,
                     ),
@@ -164,36 +270,13 @@ class _EventsScreenState extends State<EventsScreen> {
                     mapType: MapType.normal,
                     myLocationButtonEnabled: true,
                     mapToolbarEnabled: true,
-                    markers: markers,
+                    markers: Set<Marker>.from(yourMarkersList),
                     buildingsEnabled: true,
                     compassEnabled: true,
                     zoomGesturesEnabled: true,
                     scrollGesturesEnabled: true,
                     layoutDirection: TextDirection.ltr,
-                    polylines: {
-                      Polyline(
-                        polylineId: const PolylineId('route'),
-                        jointType: JointType.mitered,
-                        consumeTapEvents: true,
-                        color: AppColors.primary,
-                        width: 5,
-                        points: latitude.isEmpty
-                            ? [
-                                LatLng(currentLat, currentLong),
-                                LatLng(currentLat, currentLong),
-                              ]
-                            : latitude
-                                .asMap()
-                                .map((index, value) => MapEntry(
-                                    index,
-                                    LatLng(
-                                      latitude[index],
-                                      longitude[index],
-                                    )))
-                                .values
-                                .toList(),
-                      ),
-                    },
+                    polylines: Set<Polyline>.from(yourPolylinesList),
                   ),
                 ),
               ],
