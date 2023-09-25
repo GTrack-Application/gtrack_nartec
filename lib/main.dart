3f102b3ca8299d675ac62463b7c9b9f711cf8fcc
faysalzaman <faysalzaman00@gmail.com>
2023-09-25 12:35:15 +0500

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:gtrack_mobile_app/global/themes/themes.dart';
 import 'package:gtrack_mobile_app/old/pages/login/user_login_page.dart';
 import 'package:gtrack_mobile_app/old/providers/dispatch_management/gln_provider.dart';
 import 'package:gtrack_mobile_app/old/providers/login/login_provider.dart';
-import 'package:gtrack_mobile_app/screens/home/capture/MappingRFID/mapping_rfid_screen.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:provider/provider.dart';
 import 'package:velocity_x/velocity_x.dart';
