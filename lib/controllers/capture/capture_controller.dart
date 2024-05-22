e2d8be9a98bed672be224f4b5969ebc9f051bd42
Wasim <wasimxaman13@gmail.com>
2024-05-22 21:02:50 +0500

    chore: Refactor capture_controller.dart for improved code organization

--- lib/controllers/capture/capture_controller.dart
+++ lib/controllers/capture/capture_controller.dart
@@ -7,22 +7,15 @@ import 'package:http/http.dart' as http;
 class CaptureController {
   Future<List<SerializationModel>> getSerializationData(gtin) async {
     String url = "${AppUrls.baseUrlWith7000}/getAllRecordsByGTIN?GTIN=$gtin";
-    print("url: $url");
-
     final uri = Uri.parse(url);
-
     var response = await http.get(uri);
-
     if (response.statusCode == 200) {
-      print("Status Code: ${response.statusCode}");
-
       var data = json.decode(response.body)['data'] as List;
 
       List<SerializationModel> serializationData =
           data.map((e) => SerializationModel.fromJson(e)).toList();
       return serializationData;
     } else {
-      print("Status Code: ${response.statusCode}");
       var data = json.decode(response.body);
       var msg = data['message'];
       throw Exception(msg);
--- lib/screens/home/capture/Serialization/serialization_screen.dart
+++ lib/screens/home/capture/Serialization/serialization_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gtrack_mobile_app/blocs/capture/association/transfer/capture_cubit.dart';
+import 'package:gtrack_mobile_app/global/common/colors/app_colors.dart';
 import 'package:searchable_listview/searchable_listview.dart';
 
 class SerializationScreen extends StatelessWidget {
@@ -11,89 +12,163 @@ class SerializationScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Serialization"),
+        backgroundColor: AppColors.pink,
+        foregroundColor: AppColors.white,
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    height: 45,
-                    child: TextField(
-                      onChanged: (value) {
-                        CaptureCubit.get(context).gtin = value;
-                      },
-                      decoration: InputDecoration(
-                        hintText: 'Enter GTIN',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 8),
-                // rounded button
-                CircleAvatar(
-                  backgroundColor: Colors.blue,
-                  foregroundColor: Colors.white,
-                  child: IconButton(
-                    icon: const Icon(Icons.search),
-                    onPressed: () {
-                      CaptureCubit.get(context).getSerializationData();
-                    },
-                  ),
-                ),
-              ],
-            ),
+            _buildSearchBar(context),
             const SizedBox(height: 8),
-            Expanded(
-              child: BlocConsumer<CaptureCubit, CaptureState>(
-                listener: (context, state) {
-                  if (state is CaptureSerializationSuccess) {
-                    CaptureCubit.get(context).serializationData = state.data;
-                  }
-                },
-                builder: (context, state) {
-                  if (state is CaptureSerializationLoading) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (state is CaptureSerializationError) {
-                    return Center(child: Text(state.message));
-                  }
+            _buildGTINText(context),
+            _buildSerializationList(context),
+            _buildCreateSerialsButton(),
+          ],
+        ),
+      ),
+    );
+  }
 
-                  return SearchableList(
-                    filter: (query) => CaptureCubit.get(context)
-                        .serializationData
-                        .where((item) {
-                      // Filter it based on the following fields
-                      /*
-                          1. Batch No.
-                          2. Expiry Date
-                          3. Serial
-                      */
-                      return item.bATCH!.contains(query) ||
-                          item.eXPIRYDATE!.contains(query) ||
-                          item.serialNo!.contains(query);
-                    }).toList(),
-                    inputDecoration: const InputDecoration(
-                      hintText: 'Search',
-                      prefixIcon: Icon(Icons.search),
+  Widget _buildSearchBar(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: SizedBox(
+            height: 45,
+            child: TextField(
+              onChanged: (value) {
+                CaptureCubit.get(context).gtin = value;
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter GTIN',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 8),
+        CircleAvatar(
+          backgroundColor: AppColors.pink,
+          foregroundColor: AppColors.white,
+          child: IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              CaptureCubit.get(context).getSerializationData();
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildGTINText(BuildContext context) {
+    return BlocBuilder<CaptureCubit, CaptureState>(
+      builder: (context, state) {
+        final gtin = CaptureCubit.get(context).gtin;
+        if (gtin.isNotEmpty) {
+          return Text(
+            "GTIN: $gtin",
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+              color: AppColors.primary,
+            ),
+          );
+        }
+        return const SizedBox.shrink();
+      },
+    );
+  }
+
+  Widget _buildSerializationList(BuildContext context) {
+    return Expanded(
+      flex: 4,
+      child: BlocConsumer<CaptureCubit, CaptureState>(
+        listener: (context, state) {
+          if (state is CaptureSerializationSuccess) {
+            CaptureCubit.get(context).serializationData = state.data;
+          }
+        },
+        builder: (context, state) {
+          if (state is CaptureSerializationLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is CaptureSerializationError) {
+            return Center(child: Text(state.message));
+          }
+
+          final serializationData = CaptureCubit.get(context).serializationData;
+          if (serializationData.isNotEmpty) {
+            return Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: AppColors.primary),
+              ),
+              child: SearchableList(
+                shrinkWrap: true,
+                filter: (query) {
+                  return serializationData.where((item) {
+                    return item.bATCH!.contains(query) ||
+                        item.eXPIRYDATE!.contains(query) ||
+                        item.serialNo!.contains(query);
+                  }).toList();
+                },
+                inputDecoration: const InputDecoration(
+                  hintText: 'Search',
+                  prefixIcon: Icon(Icons.search),
+                ),
+                initialList: serializationData,
+                itemBuilder: (item) {
+                  final index = serializationData.indexOf(item) + 1;
+                  return Container(
+                    height: 40,
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
                     ),
-                    initialList: CaptureCubit.get(context).serializationData,
-                    itemBuilder: (item) => Card(
-                      child: ListTile(
-                        title: Text(item.bATCH.toString()),
-                      ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: AppColors.skyBlue),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("$index", style: const TextStyle(fontSize: 12)),
+                        Text("${item.serialNo}",
+                            style: const TextStyle(fontSize: 12)),
+                        const SizedBox.shrink(),
+                      ],
                     ),
                   );
                 },
               ),
+            );
+          }
+          return const SizedBox.shrink();
+        },
+      ),
+    );
+  }
+
+  Widget _buildCreateSerialsButton() {
+    return Expanded(
+      child: Row(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              // Implement the create serials functionality
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.pink,
+              foregroundColor: AppColors.white,
             ),
-          ],
-        ),
+            child: const Text("Create Serials"),
+          ),
+        ],
       ),
     );
   }
