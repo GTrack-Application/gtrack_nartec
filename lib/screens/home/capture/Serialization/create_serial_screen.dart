56f2a81f5ea403806a1a6d9551a21d85adee2ee6
faysalzaman <faysalzaman00@gmail.com>
2024-06-05 17:05:01 +0500

    Updated

--- lib/screens/home/capture/Serialization/create_serial_screen.dart
+++ lib/screens/home/capture/Serialization/create_serial_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gtrack_mobile_app/cubit/capture/capture_cubit.dart';
 import 'package:gtrack_mobile_app/global/common/colors/app_colors.dart';
-import 'package:gtrack_mobile_app/global/common/utils/app_snakbars.dart';
 import 'package:nb_utils/nb_utils.dart';
 
 class CreateSerialScreen extends StatefulWidget {
