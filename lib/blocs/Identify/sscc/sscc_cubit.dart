3f57771c09ca21edbcd4faf909807ca3f47acff2
Wasim Zaman <wasimxaman13@gmail.com>
2025-04-10 16:38:04 +0500

    feat: Enhance SSCC functionality and UI improvements
    - Added ssccList to SsccCubit for better state management of SSCC data.
    - Updated SsccController to correctly handle HTTP response data.
    - Refactored SsccProductsScreen to utilize AppSnackbars for error handling and improve UI responsiveness.
    - Introduced loading and empty state views for better user feedback during data retrieval.

--- lib/blocs/Identify/sscc/sscc_cubit.dart
+++ lib/blocs/Identify/sscc/sscc_cubit.dart
@@ -1,15 +1,23 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gtrack_nartec/blocs/Identify/sscc/sscc_states.dart';
 import 'package:gtrack_nartec/controllers/Identify/SSCC/SsccController.dart';
+import 'package:gtrack_nartec/models/IDENTIFY/SSCC/SsccModel.dart';
 
 class SsccCubit extends Cubit<SsccState> {
   SsccCubit() : super(SsccInitState());
+
+  static SsccCubit get(BuildContext context) => BlocProvider.of(context);
+
+  // Lists
+  List<SsccModel> ssccList = [];
 
   void getSsccData() async {
     emit(SsccLoadingState());
 
     try {
       final data = await SsccController.getProducts();
+      ssccList = data;
       emit(SsccLoadedState(data: data));
     } catch (e) {
       emit(SsccErrorState(message: e.toString()));
