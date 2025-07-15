326188096a7cf54ab73006bf0069df2e95031d62
Faysal Zaman <123411654+faysalzaman@users.noreply.github.com>
2025-07-15 15:46:25 +0500

    Remove unused local variable warning suppression from PickItemsScreen

--- lib/features/capture/view/association_internal_goodsIssue_productionJobOrder/pick_items_screen.dart
+++ lib/features/capture/view/association_internal_goodsIssue_productionJobOrder/pick_items_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gtrack_nartec/features/capture/cubits/association_internal_goodsIssue_productionJobOrder/production_job_order_cubit.dart';
